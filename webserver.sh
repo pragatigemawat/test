e318e07e319f9c928b1f5dce153be7a3302c50dd
root <gemawatpragati@gmail.com>
2021-04-23 10:16:47 +0000

    This is second project

---
+++ webserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install -y httpd
+echo "<h1>This webpage has been built with the help of Jenknins</h1">
+/var/www/html/index.html
+systemctl start httpd
+systemctl status httpd
+sleep 10
+
+
